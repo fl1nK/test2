88e99cadf634439123bb1fee9237e5018c8dfd2a
= <=>
2022-11-18 20:28:40 +0200

    1

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 import http from 'node:http'
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/' && req.method === 'GET') {
-    // res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('Homepage')
+  if (req.method === 'GET') {
+    switch (req.url) {
+      case '/':
+        homepage(req, res)
+        break
+      case '/hello':
+        hello(req, res)
+        break
+      case '/hello2':
+        homepage(req, res)
+        break
+    }
   }
 })
 
+function homepage(req, res) {
+  res.end('homepage')
+}
+
+function hello(req, res) {
+  res.end('hello')
+}
 server.on('clientError', (_err, socket) => {
   socket.end('HTTP/1.1 400 Bad Request\r\n\r\n')
 })

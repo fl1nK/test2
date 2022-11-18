1277c0b4d12c6a33094cebae6c500460f2079762
= <=>
2022-11-18 20:19:33 +0200

    1

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-import http from 'node:http'
+import * as http from 'node:http'
 
 const server = http.createServer((req, res) => {
   if (req.url === '/' && req.method === 'GET') {
-    //res.writeHead(200, { 'Content-Type': 'text/plain' })
+    // res.writeHead(200, { 'Content-Type': 'text/plain' })
     res.end('Homepage')
-  } else if (req.url === '/hello' && req.method === 'GET') {
-    //res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('hello')
-  } else {
-    res.end(
-      res.writeHead(404, { 'Content-Type': 'application/json' }),
-      JSON.stringify({
-        message: 'Route Not Found',
-      })
-    )
   }
 })
 
 server.on('clientError', (_err, socket) => {
   socket.end('HTTP/1.1 400 Bad Request\r\n\r\n')
 })
-server.listen(parseInt(process.env.PORT) || 8000)
+server.listen(parseInt(process.env.PORT) || 3000)
 
 process.on('SIGINT', () => {
   server.close((error) => {

3f65adffaf08af6a7703818771fc6e02c931533d
= <=>
2022-11-18 21:28:37 +0200

    2

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-export default function handler(req, res) {
-  const { name = 'World' } = req.query
-  return res.send(`Hello ${name}!`)
-}
+import * as http from 'node:http'
+
+const server = http.createServer((req, res) => {
+  // eslint-disable-next-line eqeqeq
+  if (req.method == 'GET') {
+    switch (res.url) {
+      case '/':
+        res.end('home')
+        break
+      case '/hello':
+        res.end('hello')
+        break
+      default:
+        res.end('404')
+        break
+    }
+  }
+})
+
+server.listen(parseInt(process.env.PORT) || 3000)
+
+server.on('clientError', (_err, socket) => {
+  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n')
+})

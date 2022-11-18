c107a06f4c2591275ae2953287608318819eacc4
= <=>
2022-11-18 22:12:44 +0200

    2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import * as http from 'node:http'
 import url from 'url'
 
 const server = http.createServer((req, res) => {
-  const path = url.URL(req.url).pathname
+  const path = url.parse(req.url).pathname
 
   if (req.method === 'GET') {
     switch (path) {
@@ -13,6 +13,25 @@ const server = http.createServer((req, res) => {
       case '/api/hello':
         res.writeHead(200, { 'Content-Type': 'text/plain' })
         res.end('hello')
+        break
+      default:
+        res.writeHead(404, { 'Content-Type': 'application/json' })
+        res.end(
+          JSON.stringify({
+            message: 'Route Not Found',
+          })
+        )
+        break
+    }
+  } else if (req.method === 'POST') {
+    switch (path) {
+      case '/':
+        res.writeHead(200, { 'Content-Type': 'text/plain' })
+        res.end('home POST')
+        break
+      case '/api/hello':
+        res.writeHead(200, { 'Content-Type': 'text/plain' })
+        res.end('hello POST')
         break
       default:
         res.writeHead(404, { 'Content-Type': 'application/json' })
